1e4f4661c3efdae4b7c9c502edb8e742398acbf9
Unknown <wanteen.y@gmail.com>
2018-01-27 16:46:57 -0800

    functions for value averages and count added

--- loadDataset.js
+++ loadDataset.js
@@ -1,4 +1,6 @@
-// Reference: tutorial 9 d3 load data
+// References: 
+// tutorial 9 d3_3 load data and d3_5 process data
+// http://learnjsdata.com/summarize_data.html
 
 var url = "http://www.sfu.ca/~wanteeny/iat355/asn2/data/Video_Games_Sales_as_at_22_Dec_2016.csv";
 
@@ -20,19 +22,71 @@ var url = "http://www.sfu.ca/~wanteeny/iat355/asn2/data/Video_Games_Sales_as_at_
 // var Developer;
 // var Rating;
 
-d3.csv(url)
-	.row(function(d){
+// d3.csv(url)
+// 	.row(function(d){
 
-		// get data values of each object?, convert numbers from string to integer
-		// d.Name = d.Name;
-		// d.Platform = d.Platform;
-		// d.Year_Of_Release += d.Year_Of_Release;
+// 		// get data values of each object?, convert numbers from string to integer
+// 		// d.Name = d.Name;
+// 		// d.Platform = d.Platform;
+// 		// d.Year_Of_Release = +d.Year_Of_Release;
+// 		d.Critic_Score = +d.Critic_Score;
 
-		// return object
-		console.log(d)
-		// console.log(d.Critic_Count)
-		return d;
-	})
-	.get(function(error, data){
-		console.log(data)
-	})
+// 		// return object
+// 		// console.log(d)
+// 		// console.log(d.Critic_Count)
+// 		return d;
+// 	})
+// 	.get(function(error, data){
+// 		// console.log(data)
+// 	})
+
+
+d3.csv(url, function(data)
+{
+
+	//#3: Average Value
+	getAverage(data,'Critic_Score');
+
+	//#4: Count Dimension Criterion Matches
+	countDimensionCriteria(data, 'Platform', "DS");
+
+});
+
+
+//Get average of values for column
+function getAverage(data, itemVariable)
+{
+	//filter items for value and return value as integer
+	var valueArray = data.filter(function (d, i)
+	{
+		// if (i < 5000) //<- constraint for how many data items to go through
+		// {
+			// console.log(+value)
+			return +d[itemVariable];
+		// }
+	});
+
+	//get average value
+	var averageValue = d3.mean(valueArray, function(d) { return +d[itemVariable]});
+	
+	//print to console
+	console.log("Average of "+ itemVariable +" = " + averageValue);
+}
+
+function countDimensionCriteria(data, dimension, itemCriteria)
+{
+	var itemCount = 0;
+
+	data.forEach(function(d, i)
+	{
+		// if (i < 5000) //<- constraint for how many data items to go through
+		// {
+			if (d[dimension].startsWith(itemCriteria))
+			{
+				itemCount++;
+			}
+		// } 
+	});
+
+	console.log("Number of " + itemCriteria + " games for " + dimension + " is: " + itemCount);
+}
